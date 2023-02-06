0c4ba545b44067a147cbb927c5a78e65261e4c11
Zoren <zorenleolumosbog@gmail.com>
2023-02-06 19:06:24 +0800

    commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Good Day Everyone
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2021 <a href="https://itsourcecode.com/">Employee Management System</a>.</strong> All Rights Reserved.
+  </footer>
